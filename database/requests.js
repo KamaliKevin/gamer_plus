1f897649324138834c8fc9f7101b55bbe8e83772
KevinBGS <geisenhofkevin@gmail.com>
2024-01-28 00:08:47 +0000

    update: Index page finally has the appropiate reflected data. Category and article pages still need to be worked on

--- database/requests.js
+++ database/requests.js
@@ -13,24 +13,112 @@ const dbConfig = {
 
 // PETICIONES:
 /*
+* Consigue todos los adelantos de todos los videos
+*/
+async function getAllVideoPreviews() {
+    const dbConnection = await connectToDatabase();
+    const [rows, fields] = await dbConnection.execute(statements.videoPreviews);
+    console.log("All video previews request done");
+    return rows;
+}
+
+/*
+* Consigue toda la publicidad secundaria
+*/
+async function getAllAsideAds() {
+    const dbConnection = await connectToDatabase();
+    const [rows, fields] = await dbConnection.execute(statements.asideAds);
+    console.log("All aside ads request done");
+    return rows;
+}
+
+/*
 * Consigue todas las categorías
 */
 async function getAllCategories() {
     const dbConnection = await connectToDatabase();
-    const [rows, fields] = await dbConnection.execute(statements.allCategories);
-    console.log("Request done");
+    const [rows, fields] = await dbConnection.execute(statements.categories);
+    console.log("All categories request done");
     return rows;
 }
 
+/*
+* Consigue todos los artículos más recientes
+*/
+async function getAllLatestArticles() {
+    const dbConnection = await connectToDatabase();
+    const [rows, fields] = await dbConnection.execute(statements.latestArticles);
+    console.log("All latest articles request done");
+    return rows;
+}
 
+/*
+* Consigue todos los artículos Nintendo de lista
+*/
+async function getAllNintendoListArticles() {
+    const dbConnection = await connectToDatabase();
+    const [rows, fields] = await dbConnection.execute(statements.nintendoListArticles);
+    console.log("All Nintendo list articles request done");
+    return rows;
+}
+
+/*
+* Consigue el artículo Nintendo destacado
+*/
+async function getNintendoFeatureArticle() {
+    const dbConnection = await connectToDatabase();
+    const [rows, fields] = await dbConnection.execute(statements.nintendoFeatureArticle);
+    console.log("Nintendo feature article request done");
+    return rows[0];
+}
+
+/*
+* Consigue todos los artículos PlayStation de lista
+*/
+async function getAllPlaystationListArticles() {
+    const dbConnection = await connectToDatabase();
+    const [rows, fields] = await dbConnection.execute(statements.playstationListArticles);
+    console.log("All PlayStation list articles request done");
+    return rows;
+}
+
+/*
+* Consigue el artículo PlayStation destacado
+*/
+async function getPlaystationFeatureArticle() {
+    const dbConnection = await connectToDatabase();
+    const [rows, fields] = await dbConnection.execute(statements.playstationFeatureArticle);
+    console.log("PlayStation feature article request done");
+    return rows[0];
+}
+
+/*
+* Consigue todos los artículos esports destacados
+*/
+async function getAllEsportsFeatureArticles() {
+    const dbConnection = await connectToDatabase();
+    const [rows, fields] = await dbConnection.execute(statements.esportsFeatureArticles);
+    console.log("Esports feature articles request done");
+    return rows;
+}
+
+/*
+* Consigue todos los artículos del carrusel
+*/
+async function getAllCarouselArticles() {
+    const dbConnection = await connectToDatabase();
+    const [rows, fields] = await dbConnection.execute(statements.carouselArticles);
+    console.log("All carousel articles request done");
+    return rows;
+}
 
 /*
 * Consigue todos los artículos
 */
 async function getAllArticles() {
     const dbConnection = await connectToDatabase();
-    const [rows, fields] = await dbConnection.execute(statements.allArticles);
-    console.log("Request done");
+    const [rows, fields] = await dbConnection.execute(statements.articles);
+    console.log("All articles request done");
     return rows;
 }
 
@@ -41,10 +129,29 @@ async function getAllArticles() {
 async function getArticleById(articleId) {
     const dbConnection = await connectToDatabase();
     const [rows, fields] = await dbConnection.execute(statements.articleById, [articleId]);
-    console.log("Request done");
+    console.log("Article by ID request done");
     return rows;
 }
 
+/*
+* Consigue la publicidad principal
+*/
+async function getHeaderAd() {
+    const dbConnection = await connectToDatabase();
+    const [rows, fields] = await dbConnection.execute(statements.headerAd);
+    console.log("Header ad request done");
+    return rows[0];
+}
+
+/*
+* Consigue el logo
+*/
+async function getLogo() {
+    const dbConnection = await connectToDatabase();
+    const [rows, fields] = await dbConnection.execute(statements.logo);
+    console.log("Logo request done");
+    return rows[0];
+}
 
 /*
 * Conecta a la base de datos
@@ -64,7 +171,18 @@ async function connectToDatabase() {
 
 // EXPORTACIÓN:
 module.exports = {
+    getLogo,
+    getHeaderAd,
     getArticleById,
     getAllArticles,
-    getAllCategories
+    getAllCategories,
+    getAllCarouselArticles,
+    getAllEsportsFeatureArticles,
+    getPlaystationFeatureArticle,
+    getAllPlaystationListArticles,
+    getNintendoFeatureArticle,
+    getAllNintendoListArticles,
+    getAllLatestArticles,
+    getAllAsideAds,
+    getAllVideoPreviews
 };
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 // INSTANCIAS:
 const express = require("express");
-const { getArticleById, getAllArticles, getAllCategories } = require("./database/requests");
+const { getLogo, getHeaderAd, getArticleById, getAllArticles, getAllCategories,
+    getAllCarouselArticles, getAllEsportsFeatureArticles,
+    getPlaystationFeatureArticle, getAllPlaystationListArticles,
+    getNintendoFeatureArticle, getAllNintendoListArticles,
+    getAllLatestArticles, getAllAsideAds, getAllVideoPreviews } = require("./database/requests");
+
 const cors = require("cors");
 const axios = require("axios");
 const ejs = require("ejs");
 const path = require("path");
+const sharp = require('sharp');
 
 const app = express();
 
@@ -41,12 +47,26 @@ app.get("/", async (req, res) => {
         // Acceso a "externalData" desde la petición
         const externalData = req.externalData;
 
-        // Ejemplo de consulta a la base de datos MySQL
+        // Consultas a la base de datos MySQL:
+        const logo = await getLogo();
+        const headerAd = await getHeaderAd();
         const articles = await getAllArticles();
+        const carouselArticles = await getAllCarouselArticles();
+        const esportsFeatureArticles = await getAllEsportsFeatureArticles();
+        const playstationFeatureArticle = await getPlaystationFeatureArticle();
+        const playstationListArticles = await getAllPlaystationListArticles();
+        const nintendoFeatureArticle = await getNintendoFeatureArticle();
+        const nintendoListArticles = await getAllNintendoListArticles();
+        const latestArticles = await getAllLatestArticles();
         const categories = await getAllCategories();
+        const asideAds = await getAllAsideAds();
+        const videoPreviews = await getAllVideoPreviews();
 
         // Devolver 'index' como vista con datos
-        res.render("index", { externalData, articles, categories });
+        res.render("index", { externalData, logo, headerAd, articles, carouselArticles,
+            esportsFeatureArticles, playstationFeatureArticle, playstationListArticles,
+            nintendoFeatureArticle, nintendoListArticles, latestArticles,
+            asideAds, videoPreviews, categories });
     }
     catch (error) {
         console.error("Error in the main route:", error);
@@ -80,6 +100,7 @@ app.get("/single", async (req, res) => {
     }
 });
 
+/*
 app.get("/single/:id", async (req, res) => {
     try {
         const externalData = req.externalData;
@@ -92,6 +113,57 @@ app.get("/single/:id", async (req, res) => {
     }
     catch (error) {
         console.error("Error in the single route:", error);
+        res.status(500).send("Internal server error");
+    }
+});
+*/
+
+app.get("/images/:imageName", async (req, res) => {
+    try {
+        const imageRef = req.params.imageName;
+        const { width, height } = req.query;
+
+        const imagePath = path.join(__dirname, '/public/img/', imageRef);
+
+        // Cargar la imagen:
+        let image = sharp(imagePath);
+
+        // Comprobar la referencia y si hay ancho y alto para ajustar las dimensiones de la imagen correspondientemente:
+        if(parseInt(width) && parseInt(height)){
+            image = image.resize(parseInt(width), parseInt(height));
+        }
+        else if(imageRef.includes("logo")){
+            image = image.resize(236, 90);
+        }
+        else if(imageRef.includes("header_ad")){
+            image = image.resize(565, 75);
+        }
+        else if (imageRef.includes("carousel") || imageRef.includes("nintendo_feature") || imageRef.includes("playstation_feature")) {
+            image = image.resize(810, 629);
+        }
+        else if (imageRef.includes("esports_feature")) {
+            image = image.resize(383, 630);
+        }
+        else if (imageRef.includes("nintendo-list") || imageRef.includes("playstation-list")) {
+            image = image.resize(80, 80);
+        }
+        else if(imageRef.includes("author")){
+            image = image.resize(64, 64);
+        }
+        else if(imageRef.includes("aside-ad")){
+            image = image.resize(255, 311);
+        }
+        else if(imageRef.includes("video-thumbnail")){
+            image = image.resize(720, 400);
+        }
+
+        res.setHeader("Content-Type", "image/jpeg");
+
+        // Mandar la imagen redimensionada como respuesta:
+        image.pipe(res);
+    }
+    catch (error) {
+        console.error("Error serving the image:", error);
         res.status(500).send("Internal server error");
     }
 });
